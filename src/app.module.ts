ba135933dff54146ca0a0d8559b5a5937d6ffa81
= <skucharek2@gmail.com>
2022-11-02 22:53:56 +0100

    basic, but working users added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CatsModule } from './cats/cats.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://root:pass12345@localhost:27017'),
     CatsModule,
+    UsersModule,
   ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
