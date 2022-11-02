c103aa97ff3ae35544801c5afa1c8b453ee03e4d
= <skucharek2@gmail.com>
2022-11-02 21:07:56 +0100

    cat schema added, module adapted to that schema

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CatSchema } from './schemas/cat.schema';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: CatsModule.name, schema: CatSchema }]),
+  ],
+  controllers: [],
+  providers: [],
+})
 export class CatsModule {}
