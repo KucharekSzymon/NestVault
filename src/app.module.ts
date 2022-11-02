4f2aa142f29d4a6e8f9d0bec53d7155c8aee13a5
= <skucharek2@gmail.com>
2022-11-02 20:40:55 +0100

    mongo connected and first schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://root:pass12345@localhost:27017/'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
