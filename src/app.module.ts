0ff88826147e973ecf8fbfc9598a04733b705370
= <skucharek2@gmail.com>
2022-11-02 22:02:44 +0100

    Fully working connection to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
-import { Cat, CatSchema } from './cats/schemas/cat.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeatureAsync([
-      {
-        name: Cat.name,
-        useFactory: () => {
-          const schema = CatSchema;
-          schema.pre('save', () => {
-            console.log('Hello from pre save at Cat schema');
-          });
-          return schema;
-        },
-      },
-    ]),
-    MongooseModule.forRoot('mongodb://root:pass12345@localhost:27017/'),
+    MongooseModule.forRoot('mongodb://root:pass12345@localhost:27017'),
     CatsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
