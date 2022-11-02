5361c49ce516be9d0adf991148f2639dad5c90d1
= <skucharek2@gmail.com>
2022-11-02 20:57:27 +0100

    auto generated cats module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://root:pass12345@localhost:27017/'),
+    CatsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
