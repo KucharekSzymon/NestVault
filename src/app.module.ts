1ce4711281f185c6b6ff7436c3c937e21df0d020
= <skucharek2@gmail.com>
2022-11-07 19:36:06 +0100

    Added JWT support Basic functions w/o mongo and proper env secret

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 import { CatsModule } from './cats/cats.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -16,8 +20,10 @@ import { CatsModule } from './cats/cats.module';
       inject: [ConfigService],
     }),
     CatsModule,
+    AuthModule,
+    UsersModule,
   ],
-  controllers: [],
-  providers: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
