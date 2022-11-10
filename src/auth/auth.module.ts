17517430930b72a2c6706663a283cc3cfe9c4145
= <skucharek2@gmail.com>
2022-11-10 18:48:54 +0100

    Moved login route to Auth controller Removed all variables loaded from env file for better implementation

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,17 +4,19 @@ import { LocalStrategy } from './local.strategy';
 import { UsersModule } from '../users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
 
 @Module({
   imports: [
     UsersModule,
     PassportModule,
     JwtModule.register({
-      secret: 'asd',
+      secret: 'asd', // do zamiany na ev value
       signOptions: { expiresIn: '60s' },
     }),
   ],
   providers: [AuthService, LocalStrategy],
   exports: [AuthService],
+  controllers: [AuthController],
 })
 export class AuthModule {}
