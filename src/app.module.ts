d12957ff2f24523445e408f32483198ddecdf525
= <skucharek2@gmail.com>
2022-11-10 19:31:19 +0100

    Added mongo handling before signup & signin

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { CatsModule } from './cats/cats.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    /*MongooseModule.forRootAsync({ //temp wylaczenie zeby zrobic od nowa wedlug docsow
+    /*ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({ //temp wylaczenie zeby zrobic od nowa wedlug docsow
       imports: [ConfigModule],
       useFactory: async (config: ConfigService) => ({
         uri: config.get('mongodb.uri'),
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,10 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User, UserDocument } from './schemas/user.schema';
 
 // This should be a real class/interface representing a user entity
-export type User = any;
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const createdUser = await this.userModel.create(createUserDto);
+    return createdUser;
+  }
+  async findAll(): Promise<User[]> {
+    return this.userModel.find().exec();
+  }
+
+  async findByName(name: string): Promise<User> {
+    return this.userModel.findOne({ username: name }).exec();
+  }
+
+  async delete(id: string) {
+    const deletedUser = await this.userModel
+      .findByIdAndRemove({ _id: id })
+      .exec();
+    return deletedUser;
+  }
+}
+/*export class UsersService {
   private readonly users = [
     {
       userId: 1,
@@ -19,6 +46,6 @@ export class UsersService {
   ];
 
   async findOne(username: string): Promise<User | undefined> {
-    return this.users.find(user => user.username === username);
+    return this.users.find((user) => user.username === username);
   }
-}
+}*/
