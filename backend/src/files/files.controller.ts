c093035fcf671b99411fe499cf92ad7412b08d08
= <skucharek2@gmail.com>
2022-12-16 19:03:39 +0100

    [/] Organized unsued elements

--- backend/src/files/files.controller.ts
+++ backend/src/files/files.controller.ts
@@ -1,19 +1,13 @@
 import {
   Controller,
   Post,
-  UploadedFile,
   Body,
   UseInterceptors,
   UploadedFiles,
-  Req,
 } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { CreateFileDto } from './dto/create-file.dto';
-import {
-  FileFieldsInterceptor,
-  FileInterceptor,
-} from '@nestjs/platform-express';
-import { diskStorage } from 'multer';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { ApiTags } from '@nestjs/swagger';
 import { multerOptions } from './common/multer-options';
 
@@ -23,14 +17,8 @@ export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
   @Post('upload')
-  //@Post('/profile/:id/picture')
   @UseInterceptors(FileInterceptor('file', multerOptions))
-  uploadfile(
-    @Body() createFileDto: CreateFileDto,
-    @UploadedFiles() files,
-    @Req() req,
-  ) {
-    console.log(req.body);
+  uploadfile(@Body() createFileDto: CreateFileDto, @UploadedFiles() files) {
     return this.filesService.create(createFileDto);
   }
 }
