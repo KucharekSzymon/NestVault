085a00d217239d42b004d25f305c6c054cba3e05
Szymon Kucharek <skucharek2@gmail.com>
2023-01-09 14:40:16 +0100

    [+] Sign In / Out buttons in sign forms

--- frontend/src/components/SignIn.vue
+++ frontend/src/components/SignIn.vue
@@ -15,7 +15,10 @@
         </div>
 
         <div>
-          <button :disabled="loading">
+          <button
+            :disabled="loading"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+          >
             <span v-show="loading"> <!--Logging loading bar --></span>
             <span>Login</span>
           </button>
