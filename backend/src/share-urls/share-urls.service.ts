d428be9c48505696f0b555b80e640d65573407bb
Szymon Kucharek <skucharek2@gmail.com>
2023-03-26 23:11:09 +0200

    [-] Unused exception

--- backend/src/share-urls/share-urls.service.ts
+++ backend/src/share-urls/share-urls.service.ts
@@ -1,8 +1,4 @@
-import {
-  BadRequestException,
-  ForbiddenException,
-  Injectable,
-} from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { FilesService } from 'src/files/files.service';
