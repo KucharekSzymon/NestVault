282b813872581d173b6078ec4b8d8ca5d41e9262
Szymon Kucharek <skucharek2@gmail.com>
2023-03-26 18:40:35 +0200

    [-] Expire date from url schema

--- backend/src/share-urls/schemas/share-url.schema.ts
+++ backend/src/share-urls/schemas/share-url.schema.ts
@@ -14,10 +14,10 @@ export class ShareUrl {
   })
   owner: User;
 
-  @Prop({ type: Date, expires: 3600, default: Date.now }) // expires in 1 hour
+  @Prop({ type: Date, default: Date.now })
   createTime: Date;
 
-  @Prop({ type: Date, expires: 3600, default: Date.now }) // expires in 1 hour
+  @Prop({ type: Date, /*expires: 3600,*/ default: Date.now }) // maybe expires in 1 hour
   expireTime: Date;
 
   @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }] })
