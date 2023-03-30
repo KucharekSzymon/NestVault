0b2635736b2e567fe2fcd1d03955f3858004f62b
Szymon Kucharek <skucharek2@gmail.com>
2023-03-30 15:15:47 +0200

    [+] Vuetify init as plugin with working component

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,9 +3,20 @@ import App from "./App.vue";
 import router from "./router/router";
 import store from "./store";
 
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
 import "./assets/main.css";
 import setupInterceptors from "./services/setupInterceptors";
 
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
 setupInterceptors(store);
 
-createApp(App).use(router).use(store).mount("#app");
+createApp(App).use(router).use(store).use(vuetify).mount("#app");
---
+++ frontend/src/plugins/vuetify.js
@@ -0,0 +1,8 @@
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
+import { md3 } from "vuetify/blueprints";
+
+const vuetify = createVuetify({
+  blueprint: md3,
+  ssr: false,
+});
