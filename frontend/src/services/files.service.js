51c9994a7e936900d07adc287e8a13e21b7a693a
Kucharek Szymon <skucharek2@gmail.com>
2023-04-10 17:54:28 +0200

    [+] Better size conversion

--- frontend/src/services/files.service.js
+++ frontend/src/services/files.service.js
@@ -15,12 +15,13 @@ class FilesService {
     return api.get("/files/preview/" + fileId, { responseType: "blob" });
   }
   convertSize(size) {
-    var fileSizeInMb = size / (1024 * 1024);
-    var fileSizeInGb = size / (1024 * 1024 * 1024);
-
-    return fileSizeInGb >= 1
-      ? fileSizeInGb.toFixed(2) + "Gb"
-      : fileSizeInMb.toFixed(2) + "Mb";
+    const units = ["B", "KB", "MB", "GB", "TB"];
+    let index = 0;
+    while (size > 1024) {
+      size /= 1024;
+      index++;
+    }
+    return size != null ? `${size.toFixed(2)} ${units[index]}` : 0;
   }
 }
 
