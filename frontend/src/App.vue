a227014aa9b9c03061384567d8dc6d82de5003fe
Szymon Kucharek <skucharek2@gmail.com>
2023-04-16 13:01:31 +0200

    [+] Navbar visual impovments

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <v-app :theme="theme">
-    <v-navigation-drawer v-model="drawer" expand-on-hover rail>
+    <v-navigation-drawer
+      v-model="drawer"
+      expand-on-hover
+      rail
+      class="d-flex flex-column"
+      style="height: 100vh"
+    >
       <v-list v-if="!currentUser" nav>
         <router-link class="text-decoration-none" to="/login">
           <v-list-item
@@ -18,14 +24,17 @@
         </router-link>
       </v-list>
       <v-list v-if="currentUser" nav>
-        <router-link class="text-decoration-none" to="/user/profile">
-          <v-list-item
-            prepend-icon="fa fa-user"
-            :title="currentUser.name"
-            :subtitle="currentUser.email"
-            value=""
-          ></v-list-item>
-        </router-link>
+        <v-list-item
+          :prepend-icon="currentRole ? 'fa fa-user-shield' : 'fa fa-user'"
+          :title="currentUser.name"
+          :subtitle="currentUser.email"
+        ></v-list-item>
+        <v-list-item
+          @click.prevent="logOut"
+          prepend-icon="fa fa-right-from-bracket "
+          title="Logout"
+          value="Logout"
+        ></v-list-item>
         <v-divider />
         <router-link class="text-decoration-none" to="/files/mine">
           <v-list-item
@@ -61,7 +70,7 @@
             value="admin"
           />
         </router-link>
-        <router-link class="text-decoration-none" to="/user/dashboard">
+        <router-link class="text-decoration-none" to="/user/">
           <v-list-item
             prepend-icon="fa-solid fa-info"
             title="User panel"
@@ -69,38 +78,29 @@
           >
           </v-list-item>
         </router-link>
-        <v-divider />
+      </v-list>
+      <v-list nav class="mt-auto">
+        <v-progress-linear
+          v-model="dataUsagePercentage"
+          :indeterminate="storageLoading"
+          absolute
+          bottom
+          rounded
+          height="3"
+          :color="storageColor"
+        ></v-progress-linear>
 
-        <a v-if="currentUser" @click.prevent="logOut">
-          <v-list-item
-            prepend-icon="fa fa-right-from-bracket "
-            title="Logout"
-            value="Logout"
-          ></v-list-item>
-        </a>
-        <v-divider></v-divider>
-        <v-list-item
-          :prepend-icon="darkTheme ? 'fa fa-sun' : 'fa fa-moon'"
-          @click="setDarkTheme(!darkTheme)"
-          title="Change theme"
-        >
-        </v-list-item>
         <v-list-item
           prepend-icon="fa-regular fa-hard-drive"
           :title="convertSize(spaceUsed) + ' / ' + convertSize(spaceLimit)"
           v-if="currentUser"
         >
         </v-list-item>
-        <v-list-item height="3">
-          <v-progress-linear
-            v-model="dataUsagePercentage"
-            :indeterminate="storageLoading"
-            absolute
-            bottom
-            rounded
-            height="3"
-            :color="storageColor"
-          ></v-progress-linear>
+        <v-list-item
+          :prepend-icon="darkTheme ? 'fa fa-sun' : 'fa fa-moon'"
+          @click="setDarkTheme(!darkTheme)"
+          title="Change theme"
+        >
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
@@ -202,3 +202,10 @@ export default {
   components: { BreadcrumbsList },
 };
 </script>
+
+<style>
+.v-navigation-drawer__content {
+  display: flex;
+  flex-direction: column;
+}
+</style>
