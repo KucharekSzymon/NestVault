4c7d16aecc78d69b0dbcc395449d5672471898d1
Szymon Kucharek <skucharek2@gmail.com>
2023-04-16 22:31:54 +0200

    [+] Basic board

--- frontend/src/components/Admin/BoardAdmin.vue
+++ frontend/src/components/Admin/BoardAdmin.vue
@@ -1,36 +1,94 @@
 <template>
-  <div class="container">
-    <header>
-      <pre>
-        {{ content }}
-      </pre>
-    </header>
-  </div>
+  <v-card :loading="loading" class="pa-2">
+    <v-row>
+      <v-col>
+        <v-progress-circular
+          absolute
+          bottom
+          rounded
+          height="3"
+          size="200"
+          width="30"
+          color="teal"
+          :indeterminate="loading"
+        >
+          20 %
+        </v-progress-circular>
+        <br />
+        <br />
+      </v-col>
+      <v-col>
+        <v-card>
+          <v-btn
+            color="cyan"
+            prepend-icon="fa fa-refresh"
+            @click="fetchStats"
+            :loading="loading"
+            >Refresh stats</v-btn
+          >
+        </v-card>
+      </v-col>
+    </v-row>
+    <RouterView />
+  </v-card>
 </template>
 
 <script>
-import UserService from "../../services/user.service";
+import filesService from "../../services/files.service";
+import shareCodeService from "../../services/shareCode.service";
+import userService from "../../services/user.service";
+import { useToast } from "vue-toastification";
 
 export default {
   name: "AdminBoard",
-
-  async mounted() {
-    try {
-      const res = await UserService.getAdminBoard();
-      this.content = res.data;
-      this.loading = false;
-    } catch (e) {
-      console.log(e);
-    }
-  },
   data() {
     return {
-      content: "AdminBoard",
+      dialog: false,
+      success: false,
       loading: false,
-      items: [],
-      search: null,
-      select: null,
+      messages: [],
     };
+  },
+  mounted() {
+    this.$watch("messages", () => {
+      const toast = useToast();
+      if (this.messages) {
+        if (Array.isArray(this.messages)) {
+          this.messages.forEach((element) => {
+            this.success ? toast.success(element) : toast.error(element);
+          });
+        } else
+          this.success
+            ? toast.success(this.messages)
+            : toast.error(this.messages);
+      }
+    });
+    this.fetchStats();
+  },
+  methods: {
+    async fetchStats() {
+      this.loading = true;
+      this.loading = false;
+    },
+    async fetchUsers() {
+      try {
+      } catch (error) {
+        this.addErrors(error);
+      }
+    },
+    addErrors(error) {
+      this.success = false;
+      this.messages = (error.response &&
+      error.response.data &&
+      Array.isArray(error.response.data.message)
+        ? error.response.data.message
+        : [error.response.data.message]) || [error.message] || [
+          error.toString(),
+        ];
+    },
+    convertSize(size) {
+      return filesService.convertSize(size);
+    },
   },
 };
 </script>
