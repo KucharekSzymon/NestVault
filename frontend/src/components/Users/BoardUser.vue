41a279308e2971035d6899fad65bc32bfa585ca2
Szymon Kucharek <skucharek2@gmail.com>
2023-04-16 12:28:16 +0200

    [-] Removed basic functions [+] Toasts support [+] Better way for displaying current data

--- frontend/src/components/Users/BoardUser.vue
+++ frontend/src/components/Users/BoardUser.vue
@@ -1,40 +1,65 @@
 <template>
-  <div class="container">
-    <header>
-      <h3>{{ content }}</h3>
-    </header>
-  </div>
+  <v-card :loading="loading" class="pa-2">
+    <pre>
+    {{ user }}
+  </pre
+    >
+  </v-card>
 </template>
 
 <script>
-import UserService from "../../services/user.service";
-import eventBus from "../../common/eventBus";
+import userService from "../../services/user.service";
+import { useToast } from "vue-toastification";
 
 export default {
   name: "UserBoard",
   data() {
     return {
-      content: "",
+      success: false,
+      loading: false,
+      messages: [],
+      user: null,
     };
   },
-  mounted() {
-    UserService.getUserBoard().then(
-      (response) => {
-        this.content = response.data;
-      },
-      (error) => {
-        this.content =
-          (error.response &&
-            error.response.data &&
-            error.response.data.message) ||
-          error.message ||
-          error.toString();
-
-        if (error.response && error.response.status === 403) {
-          eventBus.dispatch("logout");
-        }
+  async mounted() {
+    this.$watch("messages", () => {
+      const toast = useToast();
+      if (this.messages) {
+        if (Array.isArray(this.messages)) {
+          this.messages.forEach((element) => {
+            this.success ? toast.success(element) : toast.error(element);
+          });
+        } else
+          this.success
+            ? toast.success(this.messages)
+            : toast.error(this.messages);
       }
-    );
+    });
+    await this.getMyData();
+  },
+  methods: {
+    async getMyData() {
+      try {
+        this.loading = true;
+        const response = await userService.getMyData();
+        this.success = true;
+        this.user = response.data;
+      } catch (error) {
+        this.success = false;
+        this.addErrors(error);
+      } finally {
+        this.loading = false;
+      }
+    },
+    addErrors(error) {
+      this.messages = (error.response &&
+      error.response.data &&
+      Array.isArray(error.response.data.message)
+        ? error.response.data.message
+        : [error.response.data.message]) || [error.message] || [
+          error.toString(),
+        ];
+    },
   },
 };
 </script>
