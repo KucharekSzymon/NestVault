5819510316074012403259d61e3b6ee2b8bac332
Kucharek Szymon <skucharek2@gmail.com>
2023-04-17 00:04:38 +0200

    [-] Unused import

--- backend/src/share-codes/share-urls.service.ts
+++ backend/src/share-codes/share-urls.service.ts
@@ -2,7 +2,6 @@ import {
   ForbiddenException,
   Injectable,
   NotFoundException,
-  UnauthorizedException,
 } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
