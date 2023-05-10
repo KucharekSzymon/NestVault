da10b118556728534d41a77e8567d49d2ede726a
Kucharek Szymon <skucharek2@gmail.com>
2023-05-10 17:42:24 +0200

    [+] wokring reCAPTCHA on register with key in env

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,8 +7,16 @@ import vuetify from "./plugins/vuetify";
 import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
 
+import { VueReCaptcha } from "vue-recaptcha-v3";
+
 import setupInterceptors from "./services/setupInterceptors";
 
 setupInterceptors(store);
 
-createApp(App).use(router).use(store).use(vuetify).use(Toast).mount("#app");
+createApp(App)
+  .use(router)
+  .use(store)
+  .use(vuetify)
+  .use(Toast)
+  .use(VueReCaptcha, { siteKey: import.meta.env.VITE_reCAPTHCA_SITE_KEY })
+  .mount("#app");
