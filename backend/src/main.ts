e6fc3b6c51941fb9dc27461174ee9cfed7d155fc
Kucharek Szymon <skucharek2@gmail.com>
2023-05-11 17:53:56 +0200

    [+] CORS settings [+] Helmet integration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,10 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const configService = app.get(ConfigService);
+
+  app.enableCors({
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    allowedHeaders: 'Content-Type,Authorization',
+  });
+  app.use(helmet());
 
   const config = new DocumentBuilder()
     .setTitle('Nest file server')
