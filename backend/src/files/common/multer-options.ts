cabf94beb4bfac9556be1ccc348eeb3b8378e8c0
Kucharek Szymon <skucharek2@gmail.com>
2023-05-16 21:24:44 +0200

    [#] File upload refactor for cleaner code

--- backend/src/files/common/multer-options.ts
+++ backend/src/files/common/multer-options.ts
@@ -9,39 +9,25 @@ export const multerConfig = {
 
 // Multer upload options
 export const multerOptions = {
-  // Enable file size limits
-  /*limits: {
-    fileSize: multerConfig.fileSize,
-  },*/
-  // Check the mimetypes to allow for upload
-  /*fileFilter: (req: any, file: any, cb: any) => {
-    if (file.mimetype.match(/\/(jpg|jpeg|png|gif|txt)$/)) {
-      // Allow storage of file
-      cb(null, true);
-    } else {
-      // Reject file
-      cb(
-        new HttpException(
-          `Unsupported file type ${extname(file.originalname)}`,
-          HttpStatus.BAD_REQUEST,
-        ),
-        false,
-      );
-    }
-  },*/
   // Storage properties
   storage: diskStorage({
-    // Destination storage path details
+    /**
+     * Destination storage path details
+     */
     destination: (req: any, file: any, cb: any) => {
       const basePath = multerConfig.dest;
       if (!existsSync(basePath)) mkdirSync(basePath);
       const uploadPath = basePath + req.user._id;
-      // Create folder if doesn't exist
+      /**
+       * Create folder if doesn't exist
+       */
       if (!existsSync(uploadPath)) mkdirSync(uploadPath);
 
       cb(null, uploadPath);
     },
-    // File modification details
+    /**
+     * File modification details
+     */
     filename: (req: any, file, cb) => {
       const name = file.originalname.split('.')[0];
       const fileExt = extname(file.originalname);
