4e9d47ea1d2e05d1377d2c747404d83c5f0140f1
Kucharek Szymon <skucharek2@gmail.com>
2023-05-23 21:46:13 +0200

    [+] Auth routes check

--- frontend/cypress/e2e/auth.cy.js
+++ frontend/cypress/e2e/auth.cy.js
@@ -1,5 +1,10 @@
 describe("Auth test", () => {
   it("Auth test", () => {
-    cy.visit("/");
+    cy.visit("/files");
+    cy.url().should("include", "login");
+    cy.visit("/user");
+    cy.url().should("include", "login");
+    cy.visit("/admin");
+    cy.url().should("include", "login");
   });
 });
